e9da48c00d7a57fd9ce8accd3c6b11d69b70f093
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-06-09 00:11:57 +0200

    improvements regarding isFinite and arithmetic functions

--- include/mathX_basic.h
+++ include/mathX_basic.h
@@ -3,27 +3,55 @@
 
 #include <iostream>
 #include "typeinfo"
+#include "iomanip"
+#include <sstream>
 
 namespace mathX {
     static int DECIMAL_ACCURACY = 3;
 
-    std::vector<std::string> valid_number_types{
-            "d", "i", "j", "s", "l", "m", "f"
-    };
-
     class basic {
     public:
+        /**
+         * adds both parameters together
+         * @param first_summand
+         * @param second_summand
+         * @return double
+         */
         static double add(const double &first_summand, const double &second_summand);
 
+        /**
+         * subtracts the second parameter from the first
+         * @param minuend
+         * @param subtrahend
+         * @return double
+         */
         static double sub(const double &minuend, const double &subtrahend);
 
+        /**
+         * Multiplies two numbers
+         * @param multiplier
+         * @param multiplicand
+         * @return double
+         */
         static double mul(const double &multiplier, const double &multiplicand);
 
+        /**
+         * Divides two numbers
+         * @param dividend
+         * @param divisor
+         * @return double
+         */
         static double div(const double &dividend, const double &divisor);
 
+        /**
+         * Checks if given type is finite
+         * @tparam T generic type that only accepts numbers
+         * @param num number
+         * @return boolean
+         */
         template<typename T>
         static bool isFinite(const T &num) {
-            if (!(std::find(valid_number_types.begin(), valid_number_types.end(), typeid(num).name()) != valid_number_types.end())) {
+            if (!std::is_arithmetic_v<T>) {
                 throw std::invalid_argument("Invalid Argument: Not valid type");
             }
             if (!std::isfinite(num)) {
--- src/mathX_basic.cpp
+++ src/mathX_basic.cpp
@@ -2,21 +2,29 @@
 
 namespace mathX {
     double basic::add(const double &first_summand, const double &second_summand) {
-        return first_summand + second_summand;
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << first_summand * second_summand;
+        return std::stod(oss.str());
     }
 
     double basic::sub(const double &minuend, const double &subtrahend) {
-        return minuend - subtrahend;
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << minuend - subtrahend;
+        return std::stod(oss.str());
     }
 
     double basic::mul(const double &multiplier, const double &multiplicand) {
-        return multiplier * multiplicand;
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << multiplier * multiplicand;
+        return std::stod(oss.str());
     }
 
     double basic::div(const double &dividend, const double &divisor) {
         if (divisor == 0) {
-            std::numeric_limits<double>::quiet_NaN();
+            return std::numeric_limits<double>::quiet_NaN();
         }
-        return dividend / divisor;
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << dividend / divisor;
+        return std::stod(oss.str());
     }
 }
