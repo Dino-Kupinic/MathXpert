1c3a4484620b7998ea89e9e23344ae77da1c0d29
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-06-09 19:35:12 +0200

    now using decimal_display singleton

--- src/mathX_basic.cpp
+++ src/mathX_basic.cpp
@@ -1,21 +1,21 @@
-#include "mathX_basic.h"
+#include "mathX_basic.hpp"
 
 namespace mathX {
     double basic::add(const double &first_summand, const double &second_summand) {
         std::ostringstream oss;
-        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << first_summand * second_summand;
+        oss << std::fixed << std::setprecision(decimal_display::get_instance().get_decimal_display()) << first_summand * second_summand;
         return std::stod(oss.str());
     }
 
     double basic::sub(const double &minuend, const double &subtrahend) {
         std::ostringstream oss;
-        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << minuend - subtrahend;
+        oss << std::fixed << std::setprecision(decimal_display::get_instance().get_decimal_display()) << minuend - subtrahend;
         return std::stod(oss.str());
     }
 
     double basic::mul(const double &multiplier, const double &multiplicand) {
         std::ostringstream oss;
-        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << multiplier * multiplicand;
+        oss << std::fixed << std::setprecision(decimal_display::get_instance().get_decimal_display()) << multiplier * multiplicand;
         return std::stod(oss.str());
     }
 
@@ -24,7 +24,7 @@ namespace mathX {
             return std::numeric_limits<double>::quiet_NaN();
         }
         std::ostringstream oss;
-        oss << std::fixed << std::setprecision(DECIMAL_ACCURACY) << dividend / divisor;
+        oss << std::fixed << std::setprecision(decimal_display::get_instance().get_decimal_display()) << dividend / divisor;
         return std::stod(oss.str());
     }
 }
