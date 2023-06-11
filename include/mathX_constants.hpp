cdd3bf168a3b3498d7b5c664bb7d5c681886641f
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-06-11 12:53:39 +0200

    add mathematical constants

---
+++ include/mathX_constants.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Dino Kupinic on 11.06.23.
+//
+
+#ifndef MATHXPERT_MATHX_CONSTANTS_HPP
+#define MATHXPERT_MATHX_CONSTANTS_HPP
+
+#include "cmath"
+
+namespace mathX {
+    static double pi = M_PI;
+    static double e = std::exp(1.0);
+    static double tau = 2 * pi;
+    static double sqrt2 = std::pow(2, 0.5);
+    static double sqrt3 = std::pow(3, 0.5);
+    static double sqrt5 = std::pow(5, 0.5);
+    static double phi = (1 + sqrt5) / 2;
+}
+
+#endif //MATHXPERT_MATHX_CONSTANTS_HPP
